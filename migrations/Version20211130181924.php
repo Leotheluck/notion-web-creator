bcba9403847bcadb7a90665404e96352e57be43f
leolargillet <llargillet@imageens.com>
2021-12-01 13:42:45 +0100

    :sparkles: Removing uid from DB

--- migrations/Version20211130181924.php
+++ migrations/Version20211130181924.php
@@ -20,7 +20,7 @@ final class Version20211130181924 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, uid INT NOT NULL, token VARCHAR(255) NOT NULL, workspace_name VARCHAR(255) NOT NULL, workspace_icon LONGTEXT DEFAULT NULL, workspace_id VARCHAR(255) NOT NULL, notion_id VARCHAR(255) NOT NULL, notion_name VARCHAR(255) NOT NULL, notion_icon LONGTEXT DEFAULT NULL, notion_email VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, token VARCHAR(255) NOT NULL, workspace_name VARCHAR(255) NOT NULL, workspace_icon LONGTEXT DEFAULT NULL, workspace_id VARCHAR(255) NOT NULL, notion_id VARCHAR(255) NOT NULL, notion_name VARCHAR(255) NOT NULL, notion_icon LONGTEXT DEFAULT NULL, notion_email VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
